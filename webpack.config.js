dabc958da0ff6c889fbef61aa4d1c56f032d9f1d
Emmanuel Ojeah <ojeahemmanuel@outlook.com>
2020-11-30 22:02:57 +0000

    Some updates!

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
 
 module.exports = {
   mode: 'development',
@@ -11,7 +18,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     publicPath: '/',
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].bundle.js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -20,9 +27,31 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
         loader: 'babel-loader',
       },
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    htmlPlugin,
+  ],
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
 };
